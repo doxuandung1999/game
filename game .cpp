514166ea91a11ce749f335008675c2508a91b2e7
doxuandung1999 <37412232+doxuandung1999@users.noreply.github.com>
2018-03-15 22:39:17 +0700

    Add files via upload

---
+++ game .cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+#include<dos.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include<cstring>
+#include<ctype.h>
+
+using namespace std;
+enum
+{
+    BLACK,
+    BLUE,
+    GREEN,
+    CYAN,
+    RED,
+    PURPLE,
+    GRAY,
+    WHITE,
+    LIGHTGRAY,
+    LIGHTBLUE,
+    LIGHTGREEN,
+    LIGHTCYAN,
+    LIGHTRED,
+    LIGHTPURPLE,
+    LIGHTYELLOW,
+    LIGHTWHITE
+};
+void gotoxy(int x, int y)
+{
+    COORD c;
+    c.X = x - 1;
+    c.Y = y - 1;
+    SetConsoleCursorPosition (GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+void clrscr()
+{
+    COORD coord;
+    DWORD written;
+    CONSOLE_SCREEN_BUFFER_INFO info;
+
+    coord.X = 0;
+    coord.Y = 0;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+    FillConsoleOutputCharacter (GetStdHandle(STD_OUTPUT_HANDLE), ' ',
+        info.dwSize.X * info.dwSize.Y, coord, &written);
+    gotoxy (1, 1);
+}
+void SetTeColor(WORD color)
+{
+    HANDLE hConsoleOutput;
+    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+    GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+    WORD wAttributes = screen_buffer_info.wAttributes;
+    color &= 0x000f;
+    wAttributes &= 0xfff0; wAttributes |= color;
+    SetConsoleTextAttribute(hConsoleOutput, wAttributes);
+}
+void SetBGColor(WORD color)
+{
+    HANDLE hConsoleOutput;
+    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    CONSOLE_SCREEN_BUFFER_INFO screen_buffer_info;
+    GetConsoleScreenBufferInfo(hConsoleOutput, &screen_buffer_info);
+
+    WORD wAttributes = screen_buffer_info.wAttributes;
+    color &= 0x000f;
+    color <<= 4;
+    wAttributes &= 0xff0f;
+    wAttributes |= color;
+
+    SetConsoleTextAttribute(hConsoleOutput, wAttributes);
+}
+void delay(int x){ Sleep(x);}
+
+
+void Nocursortype()
+{
+    CONSOLE_CURSOR_INFO Info;
+    Info.bVisible = FALSE;
+    Info.dwSize=20;
+    SetConsoleCursorInfo (GetStdHandle (STD_OUTPUT_HANDLE), &Info);
+}
+int main(){
+    gotoxy(4,4);
+    cout << "hello";
+
+
+
+}
